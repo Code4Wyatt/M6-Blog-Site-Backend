79d9452752e4e3fd5411528131e3c24294d7c3d1
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-11-09 15:00:40 +0000

    Added authorsRouter and created snippet

---
+++ src/services/authors/index.js
@@ -0,0 +1,45 @@
+import express from "express"
+
+const authorsRouter = express.Router()
+
+authorsRouter.get("/", async (req, res, next) => {
+    try {
+        
+    } catch (error){
+      next(error)
+    }
+})
+
+authorsRouter.get("/", async (req, res, next) => {
+    try {
+        
+    } catch (error){
+      next(error)
+    }
+})
+
+authorsRouter.get("/:id", async (req, res, next) => {
+    try {
+        
+    } catch (error){
+      next(error)
+    }
+})
+
+authorsRouter.put("/:id", async (req, res, next) => {
+    try {
+        
+    } catch (error){
+      next(error)
+    }
+})
+
+authorsRouter.delete("/:id", async (req, res, next) => {
+    try {
+        
+    } catch (error){
+      next(error)
+    }
+})
+
+export default authorsRouter;
