ae7b248860e00b2fa3f089e5b3acf4cc26f4b018
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-11-09 14:43:38 +0000

    Minimum server config set up

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const server = express();
+
+const port = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_CONNECTION);
+
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB Connected!");
+
+    server.listen(port, () => {
+        console.table(listEndpoints(server));
+
+        console.log("Server running on port: ${port} ");
+    })
+})
+
+mongoose.connection.on("error", err => {
+    console.log(err);
+})
